520cebe6c3aee56e0046e71c3cbe7e43dc5c0f76
Kim <kimberly.henry@outlook.com>
2022-12-08 01:44:04 -0500

    CC-2 CH01 CarlysMotto

---
+++ src/ch01/CarlysMotto.java
@@ -0,0 +1,17 @@
+package ch01;
+
+/*
+Project:           Carlys_Catering
+Module:         Carlys_Catering
+Package:        ch01
+File:               CarlysMotto
+Created By:   Kimberly Henry
+Created On:  Thursday, 08-Dec-2022
+*/
+public class CarlysMotto
+	{
+		public static void main(String[] args)
+			{
+				System.out.println("Carly's makes the food that makes it a party.");
+			}
+	}
